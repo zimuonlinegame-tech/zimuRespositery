1105ad70379a58725092b0b2243123c46f675ca6
zimuonlinegame-tech <zimuonlinegame@gmail.com>
2025-11-20 18:40:54 +0800

    Add Spring Boot 3 scaffold for caiwu service

---
+++ caiwu/src/test/java/com/example/caiwu/CaiwuApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.caiwu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaiwuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
